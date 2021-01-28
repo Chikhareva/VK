8c5cd20ebe44fd0aacc8d2e09c838ee777684759
Vera Chikhareva <vera.chepovaya@mail.ru>
2021-01-29 00:20:14 +0300

    add project

--- src/main/java/ru/netology/domain/Comments.java
+++ src/main/java/ru/netology/domain/Comments.java
@@ -4,6 +4,6 @@ public class Comments {
     private int count;// количество комментариев;
     private String canPost;//информация о том, может ли текущий пользователь комментировать запись
     private String groupsCanPost;// информация о том, могут ли сообщества комментировать запись
-    private String  signerID; // ИД пользователя который оставил запись
+    private String signerID; // ИД пользователя который оставил запись
     private String signerImage;// фото пользователя который оставил запись
 }
