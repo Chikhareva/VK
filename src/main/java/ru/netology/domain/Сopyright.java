7775fa1cb83521edcd6e53db0690bd43eb718fcd
Vera Chikhareva <vera.chepovaya@mail.ru>
2021-01-29 14:05:36 +0300

    add classes

---
+++ src/main/java/ru/netology/domain/Сopyright.java
@@ -0,0 +1,8 @@
+package ru.netology.domain;
+
+public class Сopyright {//источник материала, объект
+    private int id;
+    private String link;
+    private String name;
+    private String tipe;
+}
