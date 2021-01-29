5e490dc39f36237dfe2a928ff460e7fca400bad0
Vera Chikhareva <vera.chepovaya@mail.ru>
2021-01-29 15:45:51 +0300

    add manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[]posts;
+    public Post [] searchPost(String query){
+        return null; //поисковой запрос
+    }
+    public void deletePost (int id){//удаление поста по ИД
+
+    }
+
+}
